7a08b49178c479641e8cdfc2d7b37688803ed09c
Dmitrii Tuchs <dtuchs@gmail.com>
2022-09-12 21:43:41 +0300

    another conflict

--- src/test/java/guru/qa/ExampleTest.java
+++ src/test/java/guru/qa/ExampleTest.java
@@ -3,17 +3,15 @@ package guru.qa;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.Random;
-
 public class ExampleTest {
 
     @Test
     void exampleTest0() {
-        Assertions.assertTrue(2 >= 1);
+        Assertions.assertTrue(2 >= 10);
     }
 
     @Test
     void exampleTest1() {
-        Assertions.assertTrue(3 >= 2);
+        Assertions.assertTrue(3 >= 10);
     }
 }
